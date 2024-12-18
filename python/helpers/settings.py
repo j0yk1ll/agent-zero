120adc3c40d3c2e06a8f87e2c4c868814e1c2e24
NiVeK92 <kevinraetz1992@gmail.com>
2024-12-18 17:01:05 +0100

    added settings section for vision model

--- python/helpers/settings.py
+++ python/helpers/settings.py
@@ -692,6 +692,7 @@ def convert_out(settings: Settings) -> SettingsOutput:
             agent_section,
             chat_model_section,
             util_model_section,
+            vision_model_section,
             embed_model_section,
             stt_section,
             api_keys_section,
@@ -756,42 +757,6 @@ def normalize_settings(settings: Settings) -> Settings:
             except (ValueError, TypeError):
                 copy[key] = value  # make default instead
     return copy
-
-
-# def get_chat_model(settings: Settings | None = None) -> BaseChatModel:
-#     if not settings:
-#         settings = get_settings()
-#     return get_model(
-#         type=ModelType.CHAT,
-#         provider=ModelProvider[settings["chat_model_provider"]],
-#         name=settings["chat_model_name"],
-#         temperature=settings["chat_model_temperature"],
-#         **settings["chat_model_kwargs"],
-#     )
-
-
-# def get_utility_model(settings: Settings | None = None) -> BaseChatModel:
-#     if not settings:
-#         settings = get_settings()
-#     return get_model(
-#         type=ModelType.CHAT,
-#         provider=ModelProvider[settings["util_model_provider"]],
-#         name=settings["util_model_name"],
-#         temperature=settings["util_model_temperature"],
-#         **settings["util_model_kwargs"],
-#     )
-
-
-# def get_embedding_model(settings: Settings | None = None) -> Embeddings:
-#     if not settings:
-#         settings = get_settings()
-#     return get_model(
-#         type=ModelType.EMBEDDING,
-#         provider=ModelProvider[settings["embed_model_provider"]],
-#         name=settings["embed_model_name"],
-#         **settings["embed_model_kwargs"],
-#     )
-
 
 def _read_settings_file() -> Settings | None:
     if os.path.exists(SETTINGS_FILE):
