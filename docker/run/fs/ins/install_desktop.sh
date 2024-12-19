85e35d86225fc5cd838ace92d85ae1a0a1a99ba7
NiVeK92 <kevinraetz1992@gmail.com>
2024-12-19 12:34:15 +0100

    fixed missing pyautogui dependency

--- docker/run/fs/ins/install_desktop.sh
+++ docker/run/fs/ins/install_desktop.sh
@@ -3,14 +3,18 @@
 # Install system dependencies for GUI and desktop sharing
 apt-get update && apt-get install -y \
     xvfb \
-    openbox \
     x11-apps \
+    x11-utils \
+    x11-xserver-utils \
+    x11vnc \
+    xclip \
+    xsel \
+    openbox \
     libgl1-mesa-glx \
     libglib2.0-0 \
     libsm6 \
     libxrender1 \
     libxext6 \
     python3-tk \
-    x11vnc \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
